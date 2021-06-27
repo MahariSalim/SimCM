1d4377780f0a5e87df86a7d51109acfb42366f38
MahariSalim <85069344+MahariSalim@users.noreply.github.com>
2021-06-27 16:41:13 +0100

    Add files via upload

--- R/chainedemarkov(simulation).R
+++ R/chainedemarkov(simulation).R
@@ -1,22 +1,24 @@
-# ´chaine de markov a temps discret
+# 'chaine de markov a temps discret
 # '@export
-# '@param z vecteur numérique représentant les étapes
-# ´@param mu vecteur numérique la distribution initiale
-# ´@param p matrice de transition
-# ´@param n nombre numérique représentant le nombre de pas
+# '@param z vecteur numerique representant les ?tapes
+# '@param mu vecteur numerique la distribution initiale
+# '@param p matrice de transition
+# '@param n nombre numerique representant le nombre de pas
 
-
-Simr <- fonction ( z , mu , P , n )
+Simr  <- function ( z , mu , P , n )
 {
-  y <- c(rep( 0 , n + 1 ))
-  t < -c( 0 : n )
-  y [ 1 ] <- rdist( z , mu )
-  for ( i  in  1 : n ){
-    y [ i + 1 ] <- rdist( z , P [ y [ i ],])
+  x  <- c(rep( 0 , n  +  1 ))
+  t  <- c(seq(0:n))
+  x [ 1 ] <- rdist( z , mu )
+
+  for ( i   in   1  :  n ){
+    x [ i  +  1 ] <- rdist( z , P [ x [ i ],])
   }
-  
-  plot( t , y , type  =  " b " , main  =  " graphe des états pour tous les temps de 0 à n " , pch = 5 , xlim = c( 0 , n ), ylim = c( 0 ,length( mu ) + 1 ), xlab  =  " temps " , ylab  =  " états " , col = " bleu" )
-  par( mfrow  = c( 2 , 1 ))
-  retour ( y )
+
+  plot( t , x  )
+  return ( x )
 }
+
+
+
 
